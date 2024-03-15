f9e3664c41a74e38baf3dac72a7b492b29b91e0f
Renan Silva <renansupermanox@gmail.com>
2024-03-15 23:33:23 +0000

    circles around circles, but with a recursive implementation

--- 15-circlesAroundCircles/sketch.js
+++ 15-circlesAroundCircles/sketch.js
@@ -1,6 +1,6 @@
 let multiplier;
-let diameter = 40;
-let amount = 10;
+let diameter = 20;
+let amount = 5;
 let offset;
 let angleIncrementer;
 let colorVar = 0;
@@ -20,26 +20,19 @@ function draw(){
     multiplier = map(mouseY, height, 0, 1, 1000)
     angleIncrementer = map(mouseX, 0, width, 0, 360);
     translate(width/2, height/2);
-    generateCircles(0);
+    generateCircles(0, 0, multiplier, diameter, angleIncrementer, 3);
     colorVar = (colorVar + 1) % 360;
     fill(colorVar,100,50,1);
 }
 
-function generateCircles(){
-    for (let i = 0; i < amount; i++) {
-        let angle = i * offset + angleIncrementer;
-        let x = cos(angle) * multiplier;
-        let y = sin(angle) * multiplier;
-        generateChildCircles(x, y, multiplier/4, diameter/2);
-        circle(x, y, diameter);
-    }
-}
-
-function generateChildCircles(centerX, centerY, multiplier, diameter){
-    for (let i = 0; i < amount; i++) {
-        let angle = i * offset - angleIncrementer;
-        let x = centerX + cos(angle) * multiplier;
-        let y = centerY + sin(angle) * multiplier;
-        circle(x, y, diameter);
+function generateCircles(centerX, centerY, multiplier, diameter, angleIncrementer, depth){
+    if(depth > 0){
+        for (let i = 0; i < amount; i++) {
+            let angle = i * offset + angleIncrementer;
+            let x = centerX + cos(angle) * multiplier;
+            let y = centerY + sin(angle) * multiplier;
+            circle(x, y, diameter);
+            generateCircles(x, y, multiplier/2, diameter/1.2, -angleIncrementer, depth - 1);
+        }
     }
 }
