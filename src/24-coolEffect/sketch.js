b091c42741efab16a74a454df883415ba960dff5
Renan Silva <renansupermanox@gmail.com>
2024-06-30 18:58:09 +0100

    adding curves to coolEffect

--- src/24-coolEffect/sketch.js
+++ src/24-coolEffect/sketch.js
@@ -1,10 +1,12 @@
-let vector;
+let vectors = [];
 
 let xMultiplier = 400;
 let yMultiplier = 100;
 
+let yMultTemp = 100;
+
 let angle = 0;
-let speed = 0;
+let speed = 40;
 
 let rotateAngle = 0
 let rotateSpeed = 0;
@@ -16,24 +18,24 @@ function setup(){
     createCanvas(900, 900);
     angleMode(DEGREES);
     colorMode(HSL);
+    noFill();
     stroke(255);
     strokeWeight(2);
     background(0);
-
     vector = createVector(cos(angle) * xMultiplier,
-    sin(angle) * yMultiplier);    
-}
+                          sin(angle) * yMultiplier);
 
+    vectors.push(vector);
+}
 function mousePressed(){
+
     background(0);
 }
-
 function draw(){
-    speed       = map(mouseX, 0, width/2, 0, 25);
-    rotateSpeed = map(mouseY, width/2, 0, 0, 10);
-    
-    //background(0);
+    frameRate(144);
     translate(width/2, height/2);
+    yMultTemp = map(mouseY, 0, height, yMultiplier, xMultiplier);
+    rotateSpeed = map(mouseX, width/2, 0, 0, 3);
 
     angle = (angle + speed) % 360;
     rotateAngle += rotateSpeed;
@@ -43,18 +45,16 @@ function draw(){
 
 function drawLine(){
     stroke(colorVar, 100, 50, 1);
-    push();
-        rotate(rotateAngle);
-        
-        let xFrom = vector.x;
-        let yFrom = vector.y;
+    rotate(rotateAngle);
+    vector = createVector(cos(angle) * xMultiplier,
+                          sin(angle) * yMultTemp);
+    if(vectors.length === 4)
+        vectors.splice(0, 1);
+    vectors.push(vector);
 
-        vector = createVector(cos(angle) * xMultiplier,
-                              sin(angle) * yMultiplier);
-        
-        
-        let xTo = vector.x;
-        let yTo = vector.y;
-        line(xFrom, yFrom, xTo, yTo);
-    pop();
+    beginShape();
+    for (let i = 0; i < vectors.length; i++) {
+        curveVertex(vectors[i].x, vectors[i].y);
+    }
+    endShape();
 }
