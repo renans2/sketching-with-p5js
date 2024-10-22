73a762da6f32fbe6db2a02122d15dc133a85f03d
Renan Silva <renansupermanox@gmail.com>
2024-10-22 13:53:05 +0100

    update perlin noise waves

--- src/perlinNoiseWaves/sketch.js
+++ src/perlinNoiseWaves/sketch.js
@@ -1,8 +1,5 @@
-let offset = 200;
-let amountX, amountY;
-let m1 = 0.001;
+let offset, amountX, amountY, m1, rectHeight;
 const m2 = 0.005;
-let rectHeight = offset/4;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -17,20 +14,7 @@ function windowResized(){
 function draw() {
   background(0);
   updateVariables();
-
-  for(let i = 0; i < amountY; i++){
-    for(let j = 0; j < amountX; j++){
-      const x = j * offset;
-      const y = i * offset + rectHeight;
-      const angle = map(noise((x * m1) + frameCount * m2, (y * m1) + frameCount * m2), 0, 1, 0, 2 * TWO_PI);
-      push();
-        translate(x,y);
-        rotate(angle);
-        fill(map(angle, 0, 1.5 * TWO_PI, 0, 360), 100, 50, 1);
-        rect(0, 0, offset, rectHeight);
-      pop();
-    }
-  }
+  drawRectangles();
 }
 
 function updateVariables(){
@@ -39,4 +23,24 @@ function updateVariables(){
   amountX = width / offset;
   amountY = height / offset;
   rectHeight = offset/4;
+}
+
+function drawRectangles(){
+  for(let i = 0; i < amountY; i++){
+    for(let j = 0; j < amountX; j++){
+      const x = j * offset;
+      const y = i * offset + rectHeight;
+      const angle = map(noise((x * m1) + frameCount * m2, (y * m1) + frameCount * m2),
+                        0,
+                        1,
+                        0,
+                        2 * TWO_PI);
+      push();
+        translate(x,y);
+        rotate(angle);
+        fill(map(angle, 0, 1.5 * TWO_PI, 0, 360), 100, 50, 1);
+        rect(0, 0, offset, rectHeight);
+      pop();
+    }
+  }
 }
