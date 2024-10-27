be8f97fce786c5a01ae69c9f450693189be095f1
Renan Silva <renansupermanox@gmail.com>
2024-10-27 18:42:34 +0000

    microphone fft visualizer

--- src/microphoneVisualizer/sketch.js
+++ src/microphoneVisualizer/sketch.js
@@ -1,15 +1,14 @@
-const bands = 512;
+const bands = 1024;
 let mic, fft, offset;
-
+let started = false;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-
-    stroke(255, 0, 0);
+    noStroke();
+    colorMode(HSL);
+    // stroke(255,0,0);
     fft = new p5.FFT(0.75, bands);
-    offset = width/bands;
-
-    noLoop();
+    offset = width/(bands * 0.66);
 }
 
 function windowResized(){
@@ -17,19 +16,42 @@ function windowResized(){
 }
 
 function draw() {
-    background(0);
+    if (started) {
+        background(0,0,0,0.5);
+        // translate(width/2, height/2);
 
-    const spectrum = fft.analyze();
+        const spectrum = fft.analyze();
 
-    for (let i = 0; i < bands; i++) {
-        const val = map(spectrum[i], 0, 255, 0, height);
-        line(i * offset, height/2 - val/2, i * offset, height/2 + val/2);
+        const angleOffset = TWO_PI/(bands * 0.66);
+        // rotate(PI/2);
+        for (let i = 0; i < bands * 0.66; i++) {
+            const val = map(spectrum[i], 0, 255, 0, height * 0.66);
+            fill(map(spectrum[i], 0, 255, 90, 450), 100, 50, 1);
+            rect(i * offset, height / 2 - val / 2, offset, val);
+
+            // push();
+            // rotate(-angleOffset * i);
+            // rect(0, - 100 -val, offset, val);
+            // pop();
+            //
+            // push();
+            // rotate(angleOffset * i);
+            // rect(0, - 100 -val, offset, val);
+            // pop();
+        }
     }
 }
 
-function startMic(){
-    mic = new p5.AudioIn();
-    mic.connect(fft);
-    mic.start();
-    loop();
+function startAudio() {
+    userStartAudio().then(() => {
+        mic = new p5.AudioIn();
+        mic.connect(fft);
+        mic.start();
+        started = true;
+    });
+}
+
+function mousePressed(){
+    if(!started)
+        startAudio();
 }
