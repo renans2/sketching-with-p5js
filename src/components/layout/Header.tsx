720f4d667904c5fb0ccef0a3f9ca9929a13cb64d
Renan Silva <renansupermanox@gmail.com>
2026-02-15 15:40:45 +0000

    update Header component + styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,25 @@
-import { NavLink } from "react-router";
+import { Link, NavLink, useLocation } from "react-router";
 
 export default function Header() {
+  const { pathname } = useLocation();
+
   return (
-    <header className="bg-cyan-500">
+    <header className="bg-black text-white">
       <div className="max-w-4xl mx-auto p-4 flex items-center justify-between">
-        <h1 className="text-2xl font-bold">Sketching w/ p5.js</h1>
+        <Link to="/">
+          <h1 className="text-2xl font-bold">Sketching w/ p5.js</h1>
+        </Link>
         <nav>
           <ul className="flex gap-2">
             <li>
-              <NavLink to="/" className="p-1 bg-gray-300 rounded-sm">
-                Home
-              </NavLink>
+              {pathname !== "/" && (
+                <NavLink to="/" className="p-1 rounded-sm">
+                  Home
+                </NavLink>
+              )}
             </li>
             <li>
-              <NavLink to="/about" className="p-1 bg-gray-300 rounded-sm">
+              <NavLink to="/about" className="p-1 rounded-sm">
                 About
               </NavLink>
             </li>
