188ed8c378197eababb4baebf38d70ce31c379dc
Renan Silva <renansupermanox@gmail.com>
2026-02-15 15:44:00 +0000

    installed react-router and setup base routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { NavLink, Route, Routes } from "react-router";
 import P5Canvas from "./components/P5Canvas";
 
 function App() {
   return (
-    <main>
-      <P5Canvas />
-    </main>
+    <div>
+      <header>
+        <nav>
+          <ul className="flex gap-2">
+            <li>
+              <NavLink to="/" className="p-1 bg-gray-300 rounded-sm">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className="p-1 bg-gray-300 rounded-sm">
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route index element={<h1>Home</h1>} />
+        <Route path="about" element={<h1>About</h1>} />
+        <Route path="sketches">
+          <Route path="one" element={<P5Canvas />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
