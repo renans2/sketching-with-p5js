2fc3f09d66eda7bf4b0750f3c9ea5d86b813fe80
Renan Silva <renansupermanox@gmail.com>
2026-02-17 14:43:19 +0000

    styles on home page and added Inter font

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,25 +4,27 @@ export default function Header() {
   const { pathname } = useLocation();
 
   return (
-    <header className="bg-black text-white">
-      <div className="max-w-4xl mx-auto p-4 flex items-center justify-between">
+    <header className="bg-white text-black">
+      <div className="max-w-500 mx-auto p-4 flex items-center justify-between">
         <Link to="/">
           <h1 className="text-2xl font-bold">Sketching w/ p5.js</h1>
         </Link>
         <nav>
           <ul className="flex gap-2">
-            <li>
-              {pathname !== "/" && (
-                <NavLink to="/" className="p-1 rounded-sm">
-                  Home
+            {pathname !== "/" && (
+              <li>
+                <NavLink to="/" className="font-medium">
+                  Sketches
                 </NavLink>
-              )}
-            </li>
-            <li>
-              <NavLink to="/about" className="p-1 rounded-sm">
-                About
-              </NavLink>
-            </li>
+              </li>
+            )}
+            {pathname !== "/about" && (
+              <li>
+                <NavLink to="/about" className="font-medium">
+                  About
+                </NavLink>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,12 @@ export default function HomePage() {
   usePageTitle("Sketching w/ p5.js");
 
   return (
-    <main className="max-w-4xl w-full mx-auto p-2 space-y-2">
-      <h2>Home</h2>
-      <div className="grid grid-cols-4 gap-1">
+    <main className="max-w-500 w-full mx-auto p-2 space-y-2">
+      <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-1">
         {SKETCH_CATALOG_INFO.map((skecth) => (
           <Link
             key={skecth.path}
-            className="bg-black font-bold p-2 text-white w-full aspect-square rounded-lg"
+            className="bg-black font-bold p-2 text-white w-full aspect-square flex items-end"
             to={`/sketch/${skecth.path}`}
           >
             {skecth.title}
