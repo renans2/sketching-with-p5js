e1c176375a32e6cc969d0fa62e10acc0de9040eb
Renan Silva <renansupermanox@gmail.com>
2026-03-06 23:19:34 +0000

    added image to test as background of sketch from homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,13 +8,20 @@ export default function HomePage() {
   return (
     <main className="max-w-500 w-full mx-auto p-2 space-y-2">
       <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-1">
-        {SKETCH_CATALOG_INFO.map((skecth) => (
+        {SKETCH_CATALOG_INFO.map((sketch) => (
           <Link
-            key={skecth.path}
-            className="border-2 border-[#00ff00] font-bold p-2 text-white w-full aspect-square flex items-end"
-            to={`/sketch/${skecth.path}`}
+            key={sketch.path}
+            to={`/sketch/${sketch.path}`}
+            className="relative border-2 border-[#00ff00] w-full aspect-square flex items-end"
           >
-            {skecth.title}
+            <img
+              src="/src/assets/sketch.png"
+              alt="sketch"
+              className="absolute w-full h-full top-0 left-0"
+            />
+            <div className="bg-[linear-gradient(to_bottom,rgba(0,0,0,0.8),rgba(0,0,0,0.8)_10%,transparent_20%,transparent)] font-bold p-2 text-white z-1 absolute w-full h-full top-0 left-0">
+              <p className="">{sketch.title}</p>
+            </div>
           </Link>
         ))}
       </div>
