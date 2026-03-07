60163a1015ab23c36f07479219acc01982a783cb
Renan Silva <renansupermanox@gmail.com>
2026-03-07 14:13:31 +0000

    added images to sketches for homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,24 @@
 import { Link } from "react-router";
 import { SKETCH_CATALOG_INFO } from "../constants/sketch-catalog-info";
 import usePageTitle from "../hooks/usePageTitle";
-import sketchImg from "../assets/sketch.png";
+
+const images = import.meta.glob("../assets/*.png", { eager: true });
 
 export default function HomePage() {
   usePageTitle("Sketching w/ p5.js");
 
   return (
     <main className="max-w-500 w-full mx-auto p-2 space-y-2">
-      <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-3">
+      <div className="w-full grid grid-cols-2 md:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-1">
         {SKETCH_CATALOG_INFO.map((sketch) => (
           <Link
             key={sketch.path}
             to={`/sketch/${sketch.path}`}
-            className="relative border-2 border-[#00ff00] w-full aspect-square flex items-end hover:scale-105 hover:z-10 transition"
+            className="relative border border-[#00ff00] w-full aspect-square flex items-end hover:scale-105 hover:z-10 transition"
           >
             <img
-              src={sketchImg}
+              src={(images[`../assets/${sketch.path}.png`] as any)?.default}
+              loading="lazy"
               alt="sketch"
               className="absolute w-full h-full top-0 left-0"
             />
